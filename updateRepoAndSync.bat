32c45d7104dfb9b77a8151fea7d109f7af82fa1a
XiaoYiXiao369 <xiaoyixiao369@gmail.com>
2024-05-16 22:26:34 +0800

    更新脚本中的小狼毫版本到weasel-0.16.0

--- updateRepoAndSync.bat
+++ updateRepoAndSync.bat
@@ -1,4 +1,4 @@
 @echo off
 start "" /B /WAIT cmd /c "cd %USERPROFILE%\AppData\Roaming\Rime && git pull"
-start "" /B /WAIT "C:\Program Files (x86)\Rime\weasel-0.15.0\WeaselDeployer.exe" /deploy
-start "" /B /WAIT "C:\Program Files (x86)\Rime\weasel-0.15.0\WeaselDeployer.exe" /sync
+start "" /B /WAIT "C:\Program Files (x86)\Rime\weasel-0.16.0\WeaselDeployer.exe" /deploy
+start "" /B /WAIT "C:\Program Files (x86)\Rime\weasel-0.16.0\WeaselDeployer.exe" /sync
