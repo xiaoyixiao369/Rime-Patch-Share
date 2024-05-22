da62903e428246b146a147b44b3f00b8a406d120
XiaoYiXiao369 <xiaoyixiao369@gmail.com>
2024-05-22 10:37:35 +0800

    Delete updateRepoAndSync.template.bat

--- updateRepoAndSync.template.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-start "" /B /WAIT cmd /c "cd {user_path} && git pull"
-start "" /B /WAIT "{install_path}/WeaselDeployer.exe" /deploy
-start "" /B /WAIT "{install_path}/WeaselDeployer.exe" /sync
